7d73701e15cb1c3e7cf50881ca18d1cfd34e98be
Alexander <oleksandr.dudko@customertimes.com>
2017-03-09 18:31:09 +0200

    added AngularFireModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 import { TranslateModule } from 'ng2-translate';
 import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCWLpFHpl1_6uuGTNYv_S7qU_N0e724DA0',
+  authDomain: 'oleksandr-dudko-cv.firebaseapp.com',
+  databaseURL: 'https://oleksandr-dudko-cv.firebaseio.com',
+  storageBucket: 'oleksandr-dudko-cv.appspot.com',
+  messagingSenderId: '793368161150'
+};
 
 @NgModule({
   declarations: [
@@ -15,6 +25,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     TranslateModule.forRoot(),
     MaterialModule
   ],
