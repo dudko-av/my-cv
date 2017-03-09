0b11655cc6349248eee6afa22da9ee2728b291d8
Alexander <oleksandr.dudko@customertimes.com>
2017-03-09 18:17:39 +0200

    added translate library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { TranslateModule } from 'ng2-translate';
 import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    TranslateModule.forRoot(),
     MaterialModule
   ],
   providers: [],
